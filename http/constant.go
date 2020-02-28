a9e269f59f442bb8e134451e9c3736d5c598787c
ParvizBoymurodov <parvizboymurodov@mail.ru>
2020-02-28 21:41:09 +0300

    Universial function

---
+++ http/constant.go
@@ -0,0 +1,13 @@
+package main
+
+const (typeHtml  ="text/html"
+jpg="image/jpg"
+jpgWay="http/img/1.jpg"
+png="image/png"
+pngWay="http/img/2.png"
+pdf="application/pdf"
+txtWay="http/files/text.txt"
+pdfWay="http/files/html.pdf"
+htmlWay="http/files/html.html"
+
+)
--- http/server.go
+++ http/server.go
@@ -22,14 +22,14 @@ func main() {
 		}
 	}()
 	log.SetOutput(file)
-	const address ="0.0.0.0:9999"
+	const address = "0.0.0.0:9999"
 	log.Print("server starting")
 	listener, err := net.Listen("tcp", address)
 	if err != nil {
-		log.Fatalf("can't listen on %s: %v",address,err)
+		log.Fatalf("can't listen on %s: %v", address, err)
 	}
 	defer listener.Close()
-	for  {
+	for {
 		conn, err := listener.Accept()
 		if err != nil {
 			log.Printf("can't accept conection %v", conn)
@@ -46,10 +46,10 @@ func handleConn(conn net.Conn) {
 			log.Fatalf("can't close connection:%v", err)
 		}
 	}()
-	reader:=bufio.NewReader(conn)
+	reader := bufio.NewReader(conn)
 	requestLine, err := reader.ReadString('\n')
 	if err != nil {
-		log.Printf("can't read %v",err)
+		log.Printf("can't read %v", err)
 	}
 	log.Printf(requestLine)
 	parts := strings.Split(strings.TrimSpace(requestLine), " ")
@@ -58,135 +58,53 @@ func handleConn(conn net.Conn) {
 	}
 
 	method, request, protocol := parts[0], parts[1], parts[2]
-	//if strings.Contains(request,"?download"){
-	//	writer := bufio.NewWriter(conn)
-	//	bytes, err := ioutil.ReadFile("http/img/")
-	//	_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
-	//	_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
-	//	_, _ = writer.WriteString("Content-Type: application/octet-stream\r\n")
-	//	_, _ = writer.WriteString("Connection: Close\r\n")
-	//	_, _ = writer.WriteString("\r\n")
-	//	_, _ = writer.Write(bytes)
-	//	err = writer.Flush()
-	//	if err != nil {
-	//		log.Printf("can't sent response: %v", err)
-	//	}
-	//	log.Printf("response on: %s", request)
-	//	return
-	//}
-	if method == "GET" && request == "/" && protocol == "HTTP/1.1" {
-		writer := bufio.NewWriter(conn)
-		bytes, err := ioutil.ReadFile("http/files/file.html")
-		_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
-		_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
-		_, _ = writer.WriteString("Content-Type: text/html\r\n")
-		_, _ = writer.WriteString("Connection: Close\r\n")
-		_, _ = writer.WriteString("\r\n")
-		_, _ = writer.Write(bytes)
-		err = writer.Flush()
-		if err != nil {
-			log.Printf("can't sent response: %v", err)
-		}
-		log.Printf("response on: %s", request)
-	}
 
-	if method == "GET" && (request == "/html.html" || request == "/html.html?download" ) && protocol == "HTTP/1.1" {
-		content:="text/html"
-		if strings.Contains(request,"?download"){
-			content = "application/octet-stream"
+	if method == "GET" && protocol == "HTTP/1.1" {
+		switch request {
+		case "/":
+			WriteBuffer(conn, "http/files/file.html", request, typeHtml)
+		case "/html.html":
+			WriteBuffer(conn, htmlWay, request, typeHtml)
+		case "/html.html?download":
+			WriteBuffer(conn, htmlWay, request, typeHtml)
+		case "/2.png":
+			WriteBuffer(conn, pngWay, request, png)
+		case "/2.png?download":
+			WriteBuffer(conn, pngWay, request, png)
+		case "/1.jpg":
+			WriteBuffer(conn, jpgWay, request, jpg)
+		case "/1.jpg?download":
+			WriteBuffer(conn, jpgWay, request, jpg)
+		case "/text.txt":
+			WriteBuffer(conn, txtWay, request, typeHtml)
+		case "/text.txt?download":
+			WriteBuffer(conn, txtWay, request, typeHtml)
+		case "/html.pdf":
+			WriteBuffer(conn, pdfWay, request, pdf)
+		case "/html.pdf?download":
+			WriteBuffer(conn, pdfWay, request, pdf)
+		default:
+			return
 		}
-		writer := bufio.NewWriter(conn)
-		bytes, err := ioutil.ReadFile("http/files/html.html")
-		_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
-		_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
-		_, _ = writer.WriteString("Content-Type: "+ content+"\r\n")
-		_, _ = writer.WriteString("Connection: Close\r\n")
-		_, _ = writer.WriteString("\r\n")
-		_, _ = writer.Write(bytes)
-		err = writer.Flush()
-		if err != nil {
-			log.Printf("can't sent response: %v", err)
-		}
-		log.Printf("response on: %s", request)
-	}
 
-	if method == "GET" && (request == "/2.png" || request == "/2.png?download" ) && protocol == "HTTP/1.1" {
-		content:="image/png"
-		if strings.Contains(request,"?download"){
-			content = "application/octet-stream"
-		}
-		writer := bufio.NewWriter(conn)
-		bytes, err := ioutil.ReadFile("http/img/2.png")
-		_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
-		_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
-		_, _ = writer.WriteString("Content-Type: "+ content+"\r\n")
-		_, _ = writer.WriteString("Connection: Close\r\n")
-		_, _ = writer.WriteString("\r\n")
-		_, _ = writer.Write(bytes)
-		err = writer.Flush()
-		if err != nil {
-			log.Printf("can't sent response: %v", err)
-		}
-		log.Printf("response on: %s", request)
 	}
+}
 
-	if method == "GET" && (request == "/1.jpg" || request == "/1.jpg?download" )  && protocol == "HTTP/1.1" {
-		content:="image/jpg"
-		if strings.Contains(request,"?download"){
-			content = "application/octet-stream"
-		}
-		writer := bufio.NewWriter(conn)
-		bytes, err := ioutil.ReadFile("http/img/1.jpg")
-		_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
-		_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
-		_, _ = writer.WriteString("Content-Type: "+ content+"\r\n")
-		_, _ = writer.WriteString("Connection: Close\r\n")
-		_, _ = writer.WriteString("\r\n")
-		_, _ = writer.Write(bytes)
-		err = writer.Flush()
-		if err != nil {
-			log.Printf("can't sent response: %v", err)
-		}
-		log.Printf("response on: %s", request)
+func WriteBuffer(conn net.Conn, fileName, request, contentType string) {
+	if strings.Contains(request, "?download") {
+		contentType = "application/octet-stream"
 	}
-
-	if method == "GET" && (request == "/text.txt" || request=="/text.txt?download") && protocol == "HTTP/1.1" {
-		content:="text/html"
-		if strings.Contains(request,"?download"){
-			content = "application/octet-stream"
-		}
-		writer := bufio.NewWriter(conn)
-		bytes, err := ioutil.ReadFile("http/files/text.txt")
-		_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
-		_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
-		_, _ = writer.WriteString("Content-Type: "+ content+"\r\n")
-		_, _ = writer.WriteString("Connection: Close\r\n")
-		_, _ = writer.WriteString("\r\n")
-		_, _ = writer.Write(bytes)
-		err = writer.Flush()
-		if err != nil {
-			log.Printf("can't sent response: %v", err)
-		}
-		log.Printf("response on: %s", request)
+	writer := bufio.NewWriter(conn)
+	bytes, err := ioutil.ReadFile(fileName)
+	_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
+	_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
+	_, _ = writer.WriteString("Content-Type: " + contentType + "\r\n")
+	_, _ = writer.WriteString("Connection: Close\r\n")
+	_, _ = writer.WriteString("\r\n")
+	_, _ = writer.Write(bytes)
+	err = writer.Flush()
+	if err != nil {
+		log.Printf("can't sent response: %v", err)
 	}
-
-	if method == "GET" && (request == "/html.pdf" || request=="/html.pdf?download") && protocol == "HTTP/1.1" {
-		content:="application/pdf"
-		if strings.Contains(request,"?download"){
-			content = "application/octet-stream"
-		}
-		writer := bufio.NewWriter(conn)
-		bytes, err := ioutil.ReadFile("http/files/html.pdf")
-		_, _ = writer.WriteString("HTTP/1.1 200 OK\r\n")
-		_, _ = writer.WriteString(fmt.Sprintf("Content-Length: %d\r\n", len(bytes)))
-		_, _ = writer.WriteString("Content-Type: "+ content+"\r\n")
-		_, _ = writer.WriteString("Connection: Close\r\n")
-		_, _ = writer.WriteString("\r\n")
-		_, _ = writer.Write(bytes)
-		err = writer.Flush()
-		if err != nil {
-			log.Printf("can't sent response: %v", err)
-		}
-		log.Printf("response on: %s", request)
-	}
+	log.Printf("response on: %s", request)
 }
